7425535fcc240352d42859223b5cd02401567b4e
Augustas Pocius <nesuspameris@gmail.com>
2021-05-04 19:49:40 +0300

    test from WSL

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -10,6 +10,7 @@ const Content = (props) => {
   return (
     <div>
       <Part part={props.parts[0]}/>
+      <Part part={props.parts[0]}/>
       <Part part={props.parts[1]}/>
       <Part part={props.parts[2]}/>
     </div>
