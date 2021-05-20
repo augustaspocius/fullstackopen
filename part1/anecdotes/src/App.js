83265c94a4f34dacddaa30b9f82af68350dd4143
Augustas Pocius <nesuspameris@gmail.com>
2021-05-20 18:29:06 +0300

    1.13

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -6,6 +6,11 @@ const Button = (props) => (
   </button>
 )
 
+const Votes = (props) => {
+    return (
+      <div>has {props.votes} votes</div>
+    )
+}
 
 const App = () => {
   const anecdotes = [
@@ -16,19 +21,40 @@ const App = () => {
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
   ]
-   
+
+  const points = { 0: 1, 
+                  1: 3, 
+                  2: 4, 
+                  3: 2, 
+                  4: 2, 
+                  5: 3, 
+                  6: 4}
+  
   const [selected, setSelected] = useState(0)
+  const [copy, vote] = useState({ ...points})
 
   const generateRandomNumber = () => {
       setSelected(Math.floor(Math.random() * anecdotes.length))
   }
+
+  const voteHandler = () => {
+      copy[selected]+=1;
+      const copy2 = {...copy}
+      vote(copy2)
+      
+  }
+
+  // increment the property 2 value by one
+  // copy[2] += 1
 
   return (
     <div>
       <div>
         {anecdotes[selected]}
       </div>
-      <Button handleClick={() => setSelected(generateRandomNumber)} text="next anecdote"/>
+      <div>has {copy[selected]} votes</div>
+      <Button handleClick={voteHandler} text="vote"/>
+      <Button handleClick={generateRandomNumber} text="next anecdote"/>
     </div>
   )
 }
