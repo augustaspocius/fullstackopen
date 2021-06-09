cd0e56636a78df0741f7ed6a8a05125ebb44baba
Augustas <nesuspameris@gmail.com>
2021-06-09 23:48:14 +0300

    2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,27 +5,35 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const addPerson = (event) => {
     console.log("addPerson");
     event.preventDefault();
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newNumber
     };
 
     const contains = persons.some(person =>{
       return JSON.stringify(newPerson) === JSON.stringify(person);
     });
-    
+
     !contains ? setPersons(persons.concat(newPerson)) 
-                    : alert(`${newName} is already added to phonebook`);
+                    : alert(`${newName} ${newNumber} is already added to phonebook`);
     
     setNewName('');
+    setNewNumber('');
   }
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value);
+  }
+
+  const handlePhoneChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value);
   }
 
   return (
@@ -36,12 +44,18 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
+          number: <input value={newNumber} onChange={handlePhoneChange}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
 
-      {persons.map(person => <div key={person.name}>{person.name}</div>)}
+      {persons.map(person => <div key={person.name}>
+                                  <span>{person.name}</span>
+                                  <span> {person.number}</span>
+                              </div>)}
     </div>
   )
 }
