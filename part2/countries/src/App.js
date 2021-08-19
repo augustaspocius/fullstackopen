1ee1e22a5b5c7db9eb702dbbb5942e88598ab1c2
Augustas <nesuspameris@gmail.com>
2021-08-19 21:55:59 +0300

    2.13

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -5,18 +5,22 @@ import axios from 'axios'
 const SearchCountries = ({ search, searchHandler }) => {
   return (
     <div>
-      find countries<input value={search} onChange={searchHandler} />
+      find countries <input value={search} onChange={searchHandler} />
     </div>
   )
 }
 
+
+
 const Countries = ({ countries }) => {
+
   if (countries.length <= 10 && countries.length > 1 && countries.length > 0) {
     return (
-      countries.map(country =>
-        <div>
-          <span>{country.name}</span>
-        </div>
+      countries.map(country => {
+        return (
+          <Country country={country} />
+        )
+      }
       ))
   }
   else if (countries.length > 10) {
@@ -28,16 +32,7 @@ const Countries = ({ countries }) => {
   }
   else if (countries.length == 1) {
     return (countries.map(country =>
-      <div>
-        <h1>{country.name}</h1>
-        <div>capital {country.capital}</div>
-        <div>population {country.population}</div>
-        <h3>languages</h3>
-        <ul>
-          {country.languages.map(language => <li key={language.iso639_2}>{language.name}</li>)}
-        </ul>
-        <img src={country.flag} width="400" height="300"></img>
-      </div>
+      <CountryDetails country={country} />
     ))
   }
   else {
@@ -47,6 +42,43 @@ const Countries = ({ countries }) => {
       </div>
     )
   }
+}
+
+const Country = ({ country }) => {
+  const [showResults, setShowResults] = useState(false)
+  const onClick = () => { setShowResults(!showResults) }
+  return (
+    <div>
+      <span>{country.name} <button onClick={onClick}>Show</button></span>
+      {showResults ? <CountryDetails country={country} /> : null}
+    </div>
+  )
+}
+
+const Button = (props) => {
+  const handleClick = () => {
+
+  }
+  return (
+    <button onClick={this.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const CountryDetails = ({ country }) => {
+  return (
+    <div>
+      <h1>{country.name}</h1>
+      <div>capital {country.capital}</div>
+      <div>population {country.population}</div>
+      <h3>languages</h3>
+      <ul>
+        {country.languages.map(language => <li key={language.iso639_2}>{language.name}</li>)}
+      </ul>
+      <img src={country.flag} width="400" height="300"></img>
+    </div>
+  )
 }
 
 function App() {
